193e0dd26ba4811e3260b418de01e71297438862
Lillian Luzinsky <eyeris@tpg.com.au>
2020-04-14 10:31:11 +1000

    updating readme file

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    // image: './assets/vmSocks-green-onWhite.jpg',
-    url: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+    image: './assets/vmSocks-green-onWhite.jpg',
+    inStock: true
   }
 })
